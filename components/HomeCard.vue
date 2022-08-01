757f6897961698cdb258556abaa2b4bc77f0afed
Skoenfeldt <info@skoen.red>
2022-08-01 18:19:34 +0200

    chore: until adding meta tags

---
+++ components/HomeCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-img height="200" :src="home.images[0]" />
+    <v-card-title>
+      {{ home.title }}
+    </v-card-title>
+    <v-card-subtitle>
+      {{ home.location.address }} • {{ home.location.city }} •
+      {{ home.location.state }}
+    </v-card-subtitle>
+    <v-card-text>
+      <v-chip-group color="primary" column>
+        <v-chip>{{ home.guests }} guests</v-chip>
+        <v-chip>{{ home.pricePerNight }} / night</v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'HomeCard',
+  props: {
+    home: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="home in homes" :key="home.objectID" cols="12" md="6" xl="4">
+        <home-card :home="home" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import homes from '@/data/homes'
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    }
+  },
+}
+</script>
+
+<style scoped></style>
